a1a0a3c6977954576b28e08dcf522c9d5b1dbc64
Hugo Mendes <hugospm@al.insper.edu.br>
2018-11-28 04:40:28 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 sudo apt-get update  # To get the latest package lists
 sudo apt-get --assume-yes install python3
 sudo apt-get --assume-yes install python3-pip
+sudo apt-get update
 sudo apt-get install Pyrebase -y
 sudo apt-get install Flask -y
